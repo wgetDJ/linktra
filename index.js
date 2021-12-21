82f85df6b9a95e19242a88199ba9bb9245810813
Dibyajyoti Mishra <dibyajyotimishraiiit@gmail.com>
2021-12-21 15:32:39 +0530

    FEAT: All external, current tab links and local storage support

--- index.js
+++ index.js
@@ -1,28 +1,66 @@
-const inputElement = document.querySelector("#input-el")
+const inputEl = document.querySelector("#input-el")
 const inputBtn = document.querySelector("#input-btn")
+const tabBtn = document.querySelector("#tab-btn")
+const removeBtn = document.querySelector("#remove-btn")
+
+const listTitle = document.querySelector("#list-title")
 const ulEL = document.querySelector("#ul-el")
 
-let myLinks = []
+let linkList = []
 
-inputBtn.addEventListener("click", () => {
-    myLinks.push(inputElement.value)
-    inputElement.value = ""
-    renderLead()
-})
-
-inputElement.addEventListener("keypress", (event) => {
-    if (event.key === "Enter") {
-        myLinks.push(inputElement.value)
-        inputElement.value = ""
-        renderLead()
+const renderItems = (itemList) => {
+    listTitle.textContent = "My List"
+    let list = ""
+    for (let item of itemList) {
+        console.log(item)
+        list += `<a href="${item}" target"_blank"><li>${item}</li></a>`
     }
-})
-
-const renderLead = () => {
-    let listItems = ""
-    for (let link of myLinks) {
-        listItems += `<a href="${link}" target"_blank><li>${link}</li></a>`
-    }
-    ulEL.innerHTML = listItems
+    ulEL.innerHTML = list
 }
+
+const getItem = (urlValue) => {
+    linkList.unshift(urlValue)
+    localStorage.setItem("myList", JSON.stringify(linkList))
+}
+
+// Getting Links from input
+inputBtn.addEventListener("click", () => {
+    urlValue = inputEl.value
+    getItem(urlValue)
+    inputEl.value = ""
+    renderItems(linkList)
+})
+inputEl.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+        urlValue = inputEl.value
+        getItem(urlValue)
+        inputEl.value = ""
+        renderItems(linkList)
+    }
+})
+
+// Getting current tab link
+tabBtn.addEventListener("click", () => {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        urlValue = tabs[0].url
+        getItem(urlValue)
+        renderItems(linkList)
+    })
+})
+
+// Deleteing All links
+removeBtn.addEventListener("dblclick", () => {
+    localStorage.clear()
+    linkList = []
+    renderItems(linkList)
+})
+
+
+
+let fromLocalStorage = JSON.parse(localStorage.getItem("myList"))
+
+if(fromLocalStorage) {
+    linkList = fromLocalStorage
+}
+
 
